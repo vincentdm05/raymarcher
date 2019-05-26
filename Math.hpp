823c3e26d6c669a2ccde0fcb8461855352b8c6a1
Vincent de Marignac <vincentdm_05@yahoo.fr>
2019-05-26 14:47:02 +0300

    move vec3 funcs from math

--- Math.hpp
+++ Math.hpp
@@ -2,21 +2,11 @@
 
 #include "Common.hpp"
 
-#include "Vec3.hpp"
+#include <math.h>
 
 inline Real min(Real a, Real b)
 {
 	return a < b ? a : b;
-}
-
-inline Real min(const Vec3 &v)
-{
-	return min(v.x, min(v.y, v.z));
-}
-
-inline Vec3 min(const Vec3 &a, const Vec3 &b)
-{
-	return Vec3(a.x < b.x ? a.x : b.x, a.y < b.y ? a.y : b.y, a.z < b.z ? a.z : b.z);
 }
 
 inline Real max(Real a, Real b)
@@ -24,28 +14,8 @@ inline Real max(Real a, Real b)
 	return a > b ? a : b;
 }
 
-inline Real max(const Vec3 &v)
-{
-	return max(v.x, max(v.y, v.z));
-}
-
-inline Vec3 max(const Vec3 &a, const Vec3 &b)
-{
-	return Vec3(a.x > b.x ? a.x : b.x, a.y > b.y ? a.y : b.y, a.z > b.z ? a.z : b.z);
-}
-
-inline Vec3 abs(const Vec3 &v)
-{
-	return Vec3(abs(v.x), abs(v.y), abs(v.z));
-}
-
 inline Real clamp(Real a, Real lower, Real upper)
 {
 	return min(upper, max(lower, a));
 }
 
-inline Vec3 lerp(const Vec3 &a, const Vec3 &b, Real t)
-{
-	t = clamp(t, 0, 1);
-	return (1 - t) * a + t * b;
-}
