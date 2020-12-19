64cab5af82e62b2ec61f506c4c9761cb814443dc
Vincent de Marignac <vincentdm_05@yahoo.fr>
2020-12-20 00:21:56 +0200

    render by tiles to get a slight speedup

--- Raymarcher.hpp
+++ Raymarcher.hpp
@@ -31,6 +31,8 @@ public:
 	Raymarcher() {}
 
 	Vec3 getColour(const Ray &r, const Scene &scene) const;
+	void renderTile(const Camera &camera, const Scene &scene, const Viewport &vp, uint tileX, uint tileY, uint tileSize, Framebuffer &framebuffer) const;
+	void renderTiles(const Camera &camera, const Scene &scene, const Viewport &vp, std::atomic<uint> &counter, Framebuffer &framebuffer) const;
 	void renderPixels(const Camera &camera, const Scene &scene, const Viewport &vp, std::atomic<uint> &counter, Framebuffer &framebuffer) const;
 	void render(const Scene &scene, const Camera &camera, Framebuffer &framebuffer) const;
 	void setVisualiseIterations(bool value) { visualiseIterations = value; }
@@ -84,6 +86,73 @@ Vec3 Raymarcher::getColour(const Ray &r, const Scene &scene) const
 	return scene.background().sample(r.direction());
 }
 
+void Raymarcher::renderTile(const Camera &camera, const Scene &scene, const Viewport &vp, uint tileX, uint tileY, uint tileSize, Framebuffer &framebuffer) const
+{
+	uint tileOffsetX = tileX * tileSize;
+	uint tileOffsetY = tileY * tileSize;
+
+	uint stopX = min(tileOffsetX + tileSize, vp.width());
+	uint stopY = min(tileOffsetY + tileSize, vp.height());
+	for (uint row = tileOffsetY; row < stopY; row++)
+	{
+		for (uint col = tileOffsetX; col < stopX; col++)
+		{
+			Real u = Real(col);
+			Real v = Real(row);
+			if (samplesPerPixel > 1)
+			{
+				u += uniformRand();
+				v += uniformRand();
+			}
+			else
+			{
+				u += 0.5;
+				v += 0.5;
+			}
+			u *= vp.widthInv();
+			v *= vp.heightInv();
+			Ray r = camera.getRay(u, v);
+			Vec3 colour = getColour(r, scene);
+
+			for (uint i = 1; i < samplesPerPixel; i++)
+			{
+				u = Real(col + uniformRand()) * vp.widthInv();
+				v = Real(row + uniformRand()) * vp.heightInv();
+				r = camera.getRay(u, v);
+				colour += getColour(r, scene);
+			}
+
+			colour /= samplesPerPixel;
+			colour = 255.99 * gammaCorrect(colour);
+
+			Real colourArray[3];
+			colourArray[0] = colour.r;
+			colourArray[1] = colour.g;
+			colourArray[2] = colour.b;
+			framebuffer.store(int(col), int(row), (byte*)colourArray);
+		}
+	}
+}
+
+void Raymarcher::renderTiles(const Camera &camera, const Scene &scene, const Viewport &vp, std::atomic<uint> &counter, Framebuffer &framebuffer) const
+{
+	constexpr uint tileSize = 64;
+	uint tilesX = (vp.width() + tileSize - 1) / tileSize;
+	uint tilesY = (vp.height() + tileSize - 1) / tileSize;
+	uint tileAmount = tilesX * tilesY;
+	while (true)
+	{
+		uint tileIndex = counter++;
+		if (tileIndex >= tileAmount)
+			break;
+
+		uint tileY = tileIndex / tilesX;
+		uint tileX = tileIndex % tilesX;
+
+		renderTile(camera, scene, vp, tileX, tileY, tileSize, framebuffer);
+	}
+}
+
 void Raymarcher::renderPixels(const Camera &camera, const Scene &scene, const Viewport &vp, std::atomic<uint> &counter, Framebuffer &framebuffer) const
 {
 	uint pixelAmount = vp.width() * vp.height();
@@ -107,15 +176,15 @@ void Raymarcher::renderPixels(const Camera &camera, const Scene &scene, const Vi
 			u += 0.5;
 			v += 0.5;
 		}
-		u /= vp.width();
-		v /= vp.height();
+		u *= vp.widthInv();
+		v *= vp.heightInv();
 		Ray r = camera.getRay(u, v);
 		Vec3 colour = getColour(r, scene);
 
 		for (uint i = 1; i < samplesPerPixel; i++)
 		{
-			u = Real(col + uniformRand()) / vp.width();
-			v = Real(row + uniformRand()) / vp.height();
+			u = Real(col + uniformRand()) * vp.widthInv();
+			v = Real(row + uniformRand()) * vp.heightInv();
 			r = camera.getRay(u, v);
 			colour += getColour(r, scene);
 		}
@@ -139,9 +208,9 @@ void Raymarcher::render(const Scene &scene, const Camera &camera, Framebuffer &f
 	const uint nThreads = max(std::thread::hardware_concurrency(), 1) - 1;
 	std::future<void> futures[nThreads];
 	for (uint i = 0; i < nThreads; i++)
-		futures[i] = std::async(std::launch::async, &Raymarcher::renderPixels, this, std::ref(camera), std::ref(scene), std::ref(viewport), std::ref(renderPixelCounter), std::ref(framebuffer));
+		futures[i] = std::async(std::launch::async, &Raymarcher::renderTiles, this, std::ref(camera), std::ref(scene), std::ref(viewport), std::ref(renderPixelCounter), std::ref(framebuffer));
 
-	renderPixels(camera, scene, viewport, renderPixelCounter, framebuffer);
+	renderTiles(camera, scene, viewport, renderPixelCounter, framebuffer);
 
 	for (uint i = 0; i < nThreads; i++)
 		futures[i].get();
