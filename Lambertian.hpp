82a45980b8f921510f7e7d350daefa445dad54be
Vincent de Marignac <vincentdm_05@yahoo.fr>
2020-12-21 23:27:25 +0200

    add secondary bounces to lambertian to get occlusions

--- Lambertian.hpp
+++ Lambertian.hpp
@@ -25,11 +25,35 @@ Vec3 Lambertian::shade(const Vec3 &incoming, const HitRecord &hr, const Scene &s
 	Real epsilon = 0.0001;
 	Vec3 normal = hr.hitable->evaluateNormal(hr.point, epsilon);
 	Vec3 irradiance;
-	uint nSamples = 50;
+	uint nSamples = 20;
 	for (uint i = 0; i < nSamples; i++)
 	{
 		Vec3 sampleDirection = normal + sampleUnitSphere();
-		irradiance += scene.background().sample(sampleDirection);
+
+		Ray ray(hr.point, sampleDirection);
+		bool hit = false;
+		HitRecord rec;
+		Real dist = 0.0;
+		for (uint iteration = 0; iteration < nSamples; iteration++)
+		{
+			Vec3 point = ray.to(dist);
+			rec.t = max();
+			hit = scene.hit(point, 1e-7, rec);
+			dist += rec.t;
+
+			if (hit || dist < rec.t || dist > 10)
+				break;
+		}
+
+		if (hit)
+		{
+			if (bounces < 1)
+				irradiance += rec.hitable->getMaterial()->shade(sampleDirection, rec, scene, bounces + 1);
+		}
+		else
+		{
+			irradiance += scene.background().sample(sampleDirection);
+		}
 	}
 	irradiance /= nSamples;
 
